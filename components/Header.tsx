e4659b261db02272dbd32be93392f2b890a12587
Shizzyonchain <satoshirunes@gmail.com>
2026-07-11 22:22:54 -0400

    refactor: remove Portfolio and AllComments modules
    Deprecated unused components and cleaned up associated navigation routes and types.

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,6 @@ export const Header: React.FC<HeaderProps> = ({ darkMode, toggleTheme, onViewCha
   const navItems = [
     { label: 'HOME', view: 'home' as View },
     { label: 'VIDEOS', view: 'videos' as View },
-    { label: 'PORTFOLIO', view: 'portfolio' as View },
     { label: 'SHOP', url: 'https://shizzyunchained.printful.me/' },
     { label: 'SHIZ UNIVERSITY', view: 'shiz-university' as View },
     { 
