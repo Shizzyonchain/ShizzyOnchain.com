d3e906fe2c41c0acf8562d6d2d8a541b207c2ce1
shizzyunchained <258681860+shizzyunchained@users.noreply.github.com>
2026-08-06 12:10:39 -0400

    Unify navigation header across site

--- app/deep-dives/deep-dives-header.tsx
+++ app/deep-dives/deep-dives-header.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import { SiteHeader } from "../site-header";
 
 export function DeepDivesHeader() {
-  return <header className="deep-header">
-    <Link className="deep-brand" href="/" aria-label="Shizzy Unchained home"><img src="/shizzy-unchained-logo.svg" alt="Shizzy Unchained" /></Link>
-    <nav aria-label="Primary navigation">
-      <Link href="/">Market</Link><Link href="/subnet-news">Subnet News</Link><Link href="/bubbles">Bubbles</Link><Link href="/video">Videos</Link><Link className="active" href="/deep-dives">Deep Dives</Link><Link href="/wallet-tracker">Wallet Tracker</Link><Link href="/about">About</Link>
-    </nav>
-  </header>;
+  return <SiteHeader />;
 }
